10b614c7fc81aca4ae0f36f2b50a3fb1c8613ae0
ZLT06 <zebolts30416@gmail.com>
2025-09-09 17:15:24 -0400

    Debugging git :(

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/githubTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/githubTest.java
@@ -2,11 +2,5 @@ package org.firstinspires.ftc.teamcode;
 
 public class githubTest {
     // hi
-    // hi again
-    // third computer hi yayayayayyay
-
-    //um please work.
-    //test
-    // test
-    // changes to manager
+    // i am so sad when github doesnt work
 }
