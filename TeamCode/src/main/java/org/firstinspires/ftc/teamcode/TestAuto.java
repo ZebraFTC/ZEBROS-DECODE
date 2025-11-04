9ad82e29136a77b62bb09ca3922d3d19d46be82e
Yuvika <yuvika.kandel@gmail.com>
2025-11-04 16:31:26 -0500

    Working Auto with simple mechanisms

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous
@@ -12,6 +13,11 @@ public class TestAuto extends LinearOpMode {
     private DcMotor frontRight;
     private DcMotor backLeft;
     private DcMotor backRight;
+    private DcMotor Intake;
+    private DcMotor Transfer;
+    private DcMotor Shooter;
+    private Servo Flap;
+
     private static ElapsedTime timer = new ElapsedTime();
 
     @Override
@@ -20,17 +26,57 @@ public class TestAuto extends LinearOpMode {
         frontRight = hardwareMap.get(DcMotor.class, "FR");
         backLeft = hardwareMap.get(DcMotor.class, "BL");
         backRight = hardwareMap.get(DcMotor.class, "BR");
+
+        Flap = hardwareMap.get(Servo.class, "flap");
+        Intake = hardwareMap.get(DcMotor.class, "intake");
+        Transfer = hardwareMap.get(DcMotor.class, "transfer");
+        Shooter = hardwareMap.get(DcMotor.class, "shooter");
+
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontLeft.setPower(0.5);
-        frontRight.setPower(0.5);
-        backLeft.setPower(0.5);
-        backRight.setPower(0.5);
-        timer.reset();
-        while (opModeIsActive() && timer.seconds() < 5.0) {
-            idle();
-        }
 
+        waitForStart();
+//        timer.reset();
+//        while (opModeIsActive() && timer.seconds() < 1.5) {
+//            frontLeft.setPower(-0.5);
+//            frontRight.setPower(-0.5);
+//            backLeft.setPower(-0.5);
+//            backRight.setPower(-0.5);
+//        }
+//        frontLeft.setPower(0);
+//        frontRight.setPower(0);
+//        backLeft.setPower(0);
+//        backRight.setPower(0);
+
+        shootOneBall();
+        shootOneBall();
+        shootOneBall();
+
+    }
+
+    private void shootOneBall() {
+        timer.reset();
+        while (opModeIsActive() && timer.seconds() < 1.0) {
+            Shooter.setPower(0.9);
+        }
+        timer.reset();
+        while (opModeIsActive() && timer.seconds() < 1.0) {
+            Shooter.setPower(0.9);
+            Flap.setPosition(0.3);
+        }
+        timer.reset();
+        while (opModeIsActive() && timer.seconds() < 1.5) {
+            Shooter.setPower(0);
+            Flap.setPosition(0.05);
+        }
+        timer.reset();
+        while (opModeIsActive() && timer.seconds() < 1.5) {
+            Shooter.setPower(0);
+            Intake.setPower(-0.67);
+            Transfer.setPower(-0.67);
+        }
+        Intake.setPower(0);
+        Transfer.setPower(0);
     }
 
 }
