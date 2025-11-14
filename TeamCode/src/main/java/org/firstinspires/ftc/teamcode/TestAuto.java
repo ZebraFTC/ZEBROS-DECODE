f524ced9843bbe68317f43ffd012467e705167c2
ZLT06 <zebolts30416@gmail.com>
2025-11-13 19:34:55 -0500

    Git maybe works on this computer now - Kate

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -35,33 +35,33 @@ public class TestAuto extends LinearOpMode {
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
+
+
         waitForStart();
-//        timer.reset();
-//        while (opModeIsActive() && timer.seconds() < 1.5) {
-//            frontLeft.setPower(-0.5);
-//            frontRight.setPower(-0.5);
-//            backLeft.setPower(-0.5);
-//            backRight.setPower(-0.5);
-//        }
-//        frontLeft.setPower(0);
-//        frontRight.setPower(0);
-//        backLeft.setPower(0);
-//        backRight.setPower(0);
+
+
+        drive(-0.5, -0.5, -0.5, -0.5, 1.5, false);
 
         shootOneBall();
         shootOneBall();
         shootOneBall();
+        drive(-0.5, 0.5, -0.5, 0.5, 0.5, false);
+        drive(0.5, 0.5, 0.5, 0.5, 0.6, false );
+        drive(0.5, -0.5, 0.5, -0.5, 0.3, false);
+        drive(0.5, 0.5, 0.5, 0.5, 0.5, true );
+        drive(0.5, 0.5, 0.5, 0.5, 0.5, false );
+        drive(0.5, -0.5, -0.5, 0.5, 0.5, false );
 
     }
 
     private void shootOneBall() {
         timer.reset();
         while (opModeIsActive() && timer.seconds() < 1.0) {
-            Shooter.setPower(0.9);
+            Shooter.setPower(0.78);
         }
         timer.reset();
         while (opModeIsActive() && timer.seconds() < 1.0) {
-            Shooter.setPower(0.9);
+            Shooter.setPower(0.78);
             Flap.setPosition(0.3);
         }
         timer.reset();
@@ -77,6 +77,32 @@ public class TestAuto extends LinearOpMode {
         }
         Intake.setPower(0);
         Transfer.setPower(0);
+
+
+
+
+
     }
 
+
+    public void drive(double frontLeftPower, double frontRightPower, double backLeftPower, double backRightPower, double time, boolean intakeAndTransfer) {
+        timer.reset();
+        while (opModeIsActive() && timer.seconds() < time) {     // While the timer is running
+            frontLeft.setPower(frontLeftPower);
+            frontRight.setPower(frontRightPower);
+            backLeft.setPower(backLeftPower);
+            backRight.setPower(backRightPower);
+            if (intakeAndTransfer) {              // If set tossee4- true it runs the intake and the transfer
+                Intake.setPower(-0.67);
+                Transfer.setPower(-0.67);
+            }
+
+        }
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        Intake.setPower(0);
+        Transfer.setPower(0);
+    }
 }
