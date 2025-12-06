a1a33ba633daeb92aa88a420222eca7bb0f16ef2
Yuvika <yuvika.kandel@gmail.com>
2025-12-06 10:16:08 -0500

    vision file made

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagWebcamExample.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class AprilTagWebcamExample extends OpMode {
+    AprilTagTest aprilTagWebcam = new AprilTagTest();
+
+
+
+    @Override
+    public void init() {
+    }
+
+
+    @Override
+    public void loop(){
+
+    }
+}
